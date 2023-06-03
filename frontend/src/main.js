467ae1405e69351d00a37acc6cd16dafcad2b56a
luxphone <zy@ifl.ee>
2023-06-03 11:50:58 +0800

    init

---
+++ frontend/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from "axios"
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL
+Vue.prototype.$axios = axios
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
